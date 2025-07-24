e05833c1a083fb54ff177aa8c158d166b7c59ddc
Tyler <100341295+TChau8@users.noreply.github.com>
2025-07-23 23:00:33 -0700

    Simple Rock, Paper Scissors Game

---
+++ rockpaperscissors.js
@@ -0,0 +1,50 @@
+//Computer random logic for rock, papers, scissors
+
+// Computer Choice
+function getComputerChoice(){
+    let RPC = ["Rock","Paper","Scissors"];              // RPC choice
+    let random = Math.floor(Math.random()*3);           // Random number gen.
+    let computerChoice = RPC[random];                        // Select random RPC
+    return computerChoice
+}
+// Human Choice
+function getHumanChoice(){                              // Human Choice
+    let humanChoice = prompt("Choose Rock, Papers or Scissors");
+    humanChoice = humanChoice.toUpperCase().at(0) + humanChoice.slice(1).toLowerCase();
+    return humanChoice
+}
+
+// Initialize Scoring
+let humanScore = 0;
+let computerScore = 0;
+
+// Game
+function playRound(humanChoice, computerChoice){
+    let HC_length = humanChoice.length
+    let CC_length = computerChoice.length
+    if (HC_length == CC_length) {                          // Tie logic
+        console.log("Tie!")
+    } else if (HC_length > CC_length && HC_length < 6){    // Paper > Rock
+        console.log("You win! Paper beats Rock!");
+        humanScore++
+    } else if (HC_length < CC_length && CC_length != 5){   // Rock > Scissors
+        console.log("You win! Rock beats Scissors!");
+        humanScore++
+    } else if (HC_length > CC_length && CC_length != 4){   // Scissors > Paper
+        console.log("You win! Scissors beats Paper!");
+        humanScore++
+    } else {
+        console.log("You're a LOSER! " + computerChoice + " beats " + humanChoice + ".");
+        computerScore++
+    }
+}
+// "5" Rounds
+function playGame(){
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(),getComputerChoice());
+        console.log("Human: " + humanScore + " vs Computer: " + computerScore);
+    }
+}
+
+
+playGame()
