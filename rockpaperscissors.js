42c317a80c93606b9f6179bd1e8d52c6e068d94e
Tyler <100341295+TChau8@users.noreply.github.com>
2025-07-26 22:37:56 -0700

    Game now initializes after pressing a button

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,7 +1,12 @@
 //Computer random logic for rock, papers, scissors
 
-// Initialize Scoring
-let humanScore = 0;
+// Variables
+const rockBtn = document.querySelector(".rockBtn");
+const paperBtn = document.querySelector(".paperBtn");
+const scissorsBtn = document.querySelector(".scissorsBtn");
+
+let rounds = 0;
+let playerScore = 0;
 let computerScore = 0;
 
 // Computer Choice
@@ -12,37 +17,27 @@ function getComputerChoice(){
     return computerChoice
 }
 
-// Human Choice
-function getHumanChoice(humanChoice){
-    humanChoice = humanChoice.toUpperCase().at(0) + humanChoice.slice(1).toLowerCase();
-    return humanChoice
-}
-
-// RPS Rounds
-function playRound(humanChoice, computerChoice){
-    if (humanChoice == computerChoice) {    // Tie
-        console.log("Tie!")
+// RPS Logic
+function playRound(playerChoice){
+    computerChoice = getComputerChoice()
+    if (playerChoice == computerChoice) {    // Tie
+        console.log("Tie!");
         return
-    } else if (humanChoice == "Paper" && computerChoice == "Rock" || // Human wins
-               humanChoice == "Rock" && computerChoice == "Scissors" ||
-               humanChoice == "Scissors" && computerChoice == "Paper"){
-        console.log("You Win! " + humanChoice + " beats " + computerChoice + ".");
-        humanScore++
+    } else if (playerChoice == "Paper" && computerChoice == "Rock" || // player wins
+               playerChoice == "Rock" && computerChoice == "Scissors" ||
+               playerChoice == "Scissors" && computerChoice == "Paper"){
+        console.log("You Win! " + playerChoice + " beats " + computerChoice + ".");
+        playerScore++
+        rounds++
         return 
     } else {
-        console.log("You Lose! " + computerChoice + " beats " + humanChoice + "."); // Computer wins
+        console.log("You Lose! " + computerChoice + " beats " + playerChoice + "."); // Computer wins
         computerScore++
+        rounds++
         return
     }
 }
-
-// UI
-const rockBtn = document.querySelector("#rockBtn");
-const paperBtn = document.querySelector("#paperBtn");
-const scissorsBtn = document.querySelector("#scissorsBtn");
-
-rockBtn.addEventListener("click", () => {getHumanChoice("rock")});
-paperBtn.addEventListener("click", () => {getHumanChoice("paper")});
-scissorsBtn.addEventListener("click", () => {getHumanChoice("scissors")});
-
-//Play Game
+// Click buttons to start game
+rockBtn.addEventListener("click", () => {playRound("Rock")});
+paperBtn.addEventListener("click", () => {playRound("Paper")});
+scissorsBtn.addEventListener("click", () => {playRound("Scissors")});
