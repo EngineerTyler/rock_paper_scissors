afdfc29665b9427019e33a6a8ac639cd8109d3c0
Tyler <100341295+TChau8@users.noreply.github.com>
2025-07-26 23:04:08 -0700

    Added a scoreboard and counting logic

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,9 +1,12 @@
-//Computer random logic for rock, papers, scissors
+//RPS logic
 
 // Variables
 const rockBtn = document.querySelector(".rockBtn");
 const paperBtn = document.querySelector(".paperBtn");
 const scissorsBtn = document.querySelector(".scissorsBtn");
+const roundCount = document.querySelector(".roundCount");
+const playerScoreCount = document.querySelector(".playerScoreCount");
+const computerScoreCount = document.querySelector(".computerScoreCount");
 
 let rounds = 0;
 let playerScore = 0;
@@ -17,26 +20,33 @@ function getComputerChoice(){
     return computerChoice
 }
 
-// RPS Logic
+// RPS Logic and Scoreboard
 function playRound(playerChoice){
     computerChoice = getComputerChoice()
-    if (playerChoice == computerChoice) {    // Tie
+    if (playerChoice == computerChoice) { // Tie
         console.log("Tie!");
+        rounds++
+        roundCount.innerText = `Round ${rounds}`;
         return
     } else if (playerChoice == "Paper" && computerChoice == "Rock" || // player wins
                playerChoice == "Rock" && computerChoice == "Scissors" ||
                playerChoice == "Scissors" && computerChoice == "Paper"){
         console.log("You Win! " + playerChoice + " beats " + computerChoice + ".");
+        rounds++
         playerScore++
-        rounds++
+        playerScoreCount.innerText = `Player: ${playerScore}`;
+        roundCount.innerText = `Round ${rounds}`;
         return 
-    } else {
-        console.log("You Lose! " + computerChoice + " beats " + playerChoice + "."); // Computer wins
-        computerScore++
+    } else { // Computer wins
+        console.log("You Lose! " + computerChoice + " beats " + playerChoice + ".");
         rounds++
+        computerScore++
+        computerScoreCount.innerText = `Computer: ${computerScore}`;
+        roundCount.innerText = `Round ${rounds}`;
         return
     }
 }
+
 // Click buttons to start game
 rockBtn.addEventListener("click", () => {playRound("Rock")});
 paperBtn.addEventListener("click", () => {playRound("Paper")});
